48a701a207df7d4584250db9990956555653a1fc
Randal <85909783+randalpereiradesouza@users.noreply.github.com>
2023-09-09 21:42:04 -0300

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("supportForm").addEventListener("submit", function(event) {
-    event.preventDefault();
-    
-    const printerModel = document.getElementById("printerModel").value;
-    const issueDescription = document.getElementById("issueDescription").value;
-
-    alert("Chamado aberto com sucesso!");
-    document.getElementById("supportForm").reset();
-});
